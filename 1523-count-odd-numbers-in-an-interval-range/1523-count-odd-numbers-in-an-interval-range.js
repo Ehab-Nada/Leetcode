28e2e0b3918bb581951028c95cb0cddb7701a4f5
Ehab Nada <65372273+hopamnada@users.noreply.github.com>
2022-05-20 04:59:00 +0200

    Time: 68 ms (84.15%), Space: 41.8 MB (81.16%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var countOdds = function(low, high) {
+    if(low%2!==0) low-=1
+    if(high%2!==0) high+=1
+    return (high - low ) / 2
+};
