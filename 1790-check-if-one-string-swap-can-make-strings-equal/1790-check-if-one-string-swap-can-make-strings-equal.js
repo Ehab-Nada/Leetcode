d8d8309572598e08171ed2070db06dc8b4462236
Ehab Nada <65372273+hopamnada@users.noreply.github.com>
2022-05-22 09:06:00 +0200

    Time: 104 ms (17.72%), Space: 45.7 MB (7.60%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
@@ -3,18 +3,10 @@
  * @param {string} s2
  * @return {boolean}
  */
-var areAlmostEqual = function(s1, s2) {
-    if(s1 === s2)
-        return true
-    let st1 = s1.split('').sort().join()
-    let st2 = s2.split('').sort().join()
-    if(st1 !== st2)
-        return false
-    let c = 0
-    for(i in s1){
-        if(s1[i]!==s2[i]){
-            c+=1
-        }
-    }
-    return c === 2
-};
+const areAlmostEqual = (s1, s2) =>
+  ((a, b, c, d, ...e) => !e.length && a === c && b === d)(
+    ...[...s1].reduce(
+      (acc, curr, idx) => (curr === s2[idx] ? acc : [curr, ...acc, s2[idx]]),
+      [],
+    ),
+  );
